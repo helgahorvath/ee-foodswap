f43f338c8a2a0f99179cac504abada19b3690e8e
havszab <havaldaszab@gmail.com>
2018-11-22 11:02:58 +0100

    userdao add method implemented

--- src/main/java/com/codecool/foodswap/dao/implementation/UserDaoImpl.java
+++ src/main/java/com/codecool/foodswap/dao/implementation/UserDaoImpl.java
@@ -3,11 +3,13 @@ package com.codecool.foodswap.dao.implementation;
 import com.codecool.foodswap.dao.UserDao;
 import com.codecool.foodswap.model.User;
 
-public class UserDaoImpl implements UserDao {
+public class UserDaoImpl extends EntityManagerJPA implements UserDao {
     
     @Override
     public void add(User user) {
-        
+        transaction.begin();
+        em.persist(user);
+        transaction.commit();
     }
 
     @Override
