b9b7962c2eb698bb7196682046d83d0a1bbf7ab9
SzilardZ <szilard.zr@gmail.com>
2018-11-22 11:14:38 +0100

    register controller added, doGet method implemented

--- src/main/java/com/codecool/foodswap/controller/RegisterController.java
+++ src/main/java/com/codecool/foodswap/controller/RegisterController.java
@@ -1,6 +1,28 @@
 package com.codecool.foodswap.controller;
 
-public class RegisterController {
+import com.codecool.foodswap.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+public class RegisterController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        context.setVariable("errorMessages", new String[0]);
+        engine.process("register.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
