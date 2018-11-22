3b478c840ee8a76bb028f4f37da81d4df6fd8e43
havszab <havaldaszab@gmail.com>
2018-11-22 09:59:07 +0100

    User dao implementation setup

---
+++ src/main/java/com/codecool/foodswap/dao/implementation/UserDaoImpl.java
@@ -0,0 +1,17 @@
+package com.codecool.foodswap.dao.implementation;
+
+import com.codecool.foodswap.dao.UserDao;
+import com.codecool.foodswap.model.User;
+
+public class UserDaoImpl implements UserDao {
+    
+    @Override
+    public void add(User user) {
+        
+    }
+
+    @Override
+    public void remove(User user) {
+
+    }
+}
