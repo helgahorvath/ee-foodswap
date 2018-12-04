c1742c5136636484f4d5372e0d21f2e50057542c
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-12-04 10:41:52 +0100

    success - test add method

--- src/test/java/com.codecool.foodswap/UserDaoTest.java
+++ src/test/java/com.codecool.foodswap/UserDaoTest.java
@@ -1,22 +1,19 @@
 package com.codecool.foodswap;
 
-import com.codecool.foodswap.dao.implementation.EntityManagerJPA;
 import com.codecool.foodswap.dao.implementation.UserDaoImpl;
 import com.codecool.foodswap.model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.persistence.EntityManagerFactory;
 import java.io.IOException;
 
-import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class UserDaoTest {
 
-     private UserDaoImpl userDaoImpl;
+    private UserDaoImpl userDaoImpl;
     private User user;
 
     @BeforeEach
@@ -27,13 +24,13 @@ public class UserDaoTest {
 
     @Test
     void testUserDaoAddUser() throws IOException {
-        boolean result = true;
         userDaoImpl.add(user);
-//        User userFromTestDB = userDaoImpl.getUserById(1);
-//        assertEquals(user.getId(), userFromTestDB.getId());
+        User userFromTestDB1 = userDaoImpl.getUserById(1);
+        assertEquals(user.getId(), userFromTestDB1.getId());
     }
 
     @AfterEach
     void tearDown() {
+
     }
 }
