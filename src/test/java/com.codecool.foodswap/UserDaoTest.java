d6ab97595865c00dc22cdfb57146c4546eaa9b86
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-12-04 08:34:40 +0100

    test add method is not ready

--- src/test/java/com.codecool.foodswap/UserDaoTest.java
+++ src/test/java/com.codecool.foodswap/UserDaoTest.java
@@ -2,6 +2,7 @@ package com.codecool.foodswap;
 
 import com.codecool.foodswap.dao.implementation.EntityManagerJPA;
 import com.codecool.foodswap.dao.implementation.UserDaoImpl;
+import com.codecool.foodswap.model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,15 +10,25 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class UserDaoTest {
+
+    private UserDaoImpl userDaoImpl;
+    private User user;
+
     @BeforeEach
     void setUp() {
+        userDaoImpl = mock(UserDaoImpl.class);
+        user = new User("Robert", "Katona", "emailemil@gmail.com","123onetwothree");
     }
 
     @Test
     void testUserDaoAddUser() throws IOException {
-        UserDaoImpl userDaoImpl = mock(UserDaoImpl.class);
+        boolean result = true;
+        userDaoImpl.add(user);
+        //boolean isAddSuccesfull = userDaoImpl.verifyUser("emailemil@gmail.com", "123onetwothree");
     }
 
     @AfterEach
