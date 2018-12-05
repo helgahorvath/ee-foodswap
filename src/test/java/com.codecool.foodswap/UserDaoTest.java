2b42e4c04044ca807612bc6696b3f93034bd6d37
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-12-05 14:15:24 +0100

    success - test verify user is ready

--- src/test/java/com.codecool.foodswap/UserDaoTest.java
+++ src/test/java/com.codecool.foodswap/UserDaoTest.java
@@ -1,8 +1,11 @@
 package com.codecool.foodswap;
 
+import com.codecool.foodswap.dao.implementation.GroupDaoImpl;
 import com.codecool.foodswap.dao.implementation.UserDaoImpl;
+import com.codecool.foodswap.model.Group;
 import com.codecool.foodswap.model.User;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,20 +19,30 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserDaoTest {
 
     private UserDaoImpl userDaoImpl;
-    private User user1;
-    private User user2;
+    private GroupDaoImpl groupDaoImpl;
+    private User testUser1;
+    private User testUser2;
+    private User testCreator1;
+    private Group testGroup1;
+
 
     @BeforeEach
     void setUp() {
         userDaoImpl = UserDaoImpl.getInstance();
+        testUser1 = new User("Test1", "Test1", "testemailemil1@gmail.com","123onetwothree1");
+        testUser2 = new User("Test2", "Test2", "testemailemil2@gmail.com","123onetwothree2");
+        testCreator1 = new User("Creator1", "Creator1", "creatoremailemil3@gmail.com","123onetwothree3");
+        userDaoImpl.add(testUser1);
+        userDaoImpl.add(testUser2);
+        userDaoImpl.add(testCreator1);
+        testGroup1 = new Group("Testgroup1", testCreator1);
+
     }
 
     @Test
     void testUserDaoAddUser() throws IOException {
-        user1 = new User("Test1", "Test1", "testemailemil1@gmail.com","123onetwothree");
-        userDaoImpl.add(user1);
         User userFromTestDB1 = userDaoImpl.getUserById(1);
-        assertEquals(user1.getId(), userFromTestDB1.getId());
+        assertEquals(testUser1.getId(), userFromTestDB1.getId());
     }
 
 
@@ -42,10 +55,26 @@ public class UserDaoTest {
 
     @Test
     void testUserDaoVerifyUser() {
+        String emailGood1 = "testemailemil1@gmail.com";
+        String pswGood1 = "123onetwothree1";
+        String emailGood2 = "testemailemil2@gmail.com";
+        String pswGood2 = "123onetwothree2";
 
+        String emailWrong = "bad@gmail.com";
+        String pswWrong = "bad123";
+
+        assertEquals(userDaoImpl.getUserById(1).getId(), userDaoImpl.verifyUser(emailGood1,pswGood1));
+        assertEquals(userDaoImpl.getUserById(2).getId(), userDaoImpl.verifyUser(emailGood2,pswGood2));
+        assertThrows(NoResultException.class, () -> { userDaoImpl.verifyUser(emailWrong, pswWrong);});
+        assertThrows(NoResultException.class, () -> { userDaoImpl.verifyUser(emailWrong, pswGood1);});
     }
 
+    @Test
+    void testUserDaoJoinGroup() {
+        testUser1.joinGroup(testGroup1);
 
+
+    }
     @AfterEach
     void tearDown() {
 
