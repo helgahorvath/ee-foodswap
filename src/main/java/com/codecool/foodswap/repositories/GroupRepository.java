926f3f7f7899e7f532415744aa3602357035bcf0
helgahorvath <h.horvath302@gmail.com>
2019-01-14 14:28:06 +0100

    group findallbyname method

--- src/main/java/com/codecool/foodswap/repositories/GroupRepository.java
+++ src/main/java/com/codecool/foodswap/repositories/GroupRepository.java
@@ -1,7 +1,13 @@
 package com.codecool.foodswap.repositories;
 
+import com.codecool.foodswap.model.Food;
 import com.codecool.foodswap.model.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GroupRepository extends JpaRepository<Group, Integer> {
+
+    List<Group> findAllByName(String name);
+
 }
