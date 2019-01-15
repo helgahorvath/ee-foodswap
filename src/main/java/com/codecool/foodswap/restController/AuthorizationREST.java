92b123094fb67d59fc58c8852afbe482e4d87c48
robertkatona <30589040+robertkatona@users.noreply.github.com>
2019-01-15 13:15:01 +0100

    success - login is ready with hashed password

--- src/main/java/com/codecool/foodswap/restController/AuthorizationREST.java
+++ src/main/java/com/codecool/foodswap/restController/AuthorizationREST.java
@@ -3,6 +3,7 @@ package com.codecool.foodswap.restController;
 import com.codecool.foodswap.model.User;
 import com.codecool.foodswap.repositories.UserRepository;
 import com.codecool.foodswap.service.UserService;
+import com.codecool.foodswap.util.Bcrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +28,22 @@ public class AuthorizationREST {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public void processRegistrationForm(@RequestBody User registerUser) {
-        User newUser = new User(registerUser.getFirstName(), registerUser.getLastName(), registerUser.getEmail(), registerUser.getPassword());
+        User newUser = new User(registerUser.getFirstName(), registerUser.getLastName(), registerUser.getEmail(), Bcrypt.hashPassword(registerUser.getPassword()));
         userService.add(newUser);
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLogin() {
+        return "Login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String processLogin(@RequestBody User loginUser) {
+        User userByEmailFromDB = userRepository.findUserByEmail(loginUser.getEmail());
+        boolean verifyPassword = Bcrypt.verifyPassword(loginUser.getPassword(), userByEmailFromDB.getPassword());
+        if (verifyPassword) {
+            return "Login successful";
+        }
+        return "Login Failed";
     }
 }
